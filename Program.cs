027bc218497e3730277dace99b5c4e4e24d8aa3f
Pascal <Pascal@DESKTOP-J4CPRIB>
2023-09-11 13:24:18 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._09._11_TESTUS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
